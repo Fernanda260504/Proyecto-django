3b2ecc5aaee7d1430df05e219549f2bf58260c47
osvaldo <osvaldo@utez.edu.mx>
2025-02-17 20:29:22 -0600

    Post asincrono

--- errorPages/productos/urls.py
+++ errorPages/productos/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('agregar/',agregar_productos,name='agregar'),
+    path('agregar/',agregar_producto,name='agregar'),
     path ('ver/',ver_producto,name='ver'),
     path('',index,name='home'),
-    path('api/get/',lista_productos,name='lista')
+    path('api/get/',lista_productos,name='lista'),
+    path('api/post/', registrar_producto, name='post'),
 ]
--- errorPages/productos/views.py
+++ errorPages/productos/views.py
@@ -1,45 +1,77 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Producto
 from .forms import productoForm
 from django.http import JsonResponse
 # Create your views here.
-def agregar_productos(request):
-    #Checar si vengo desde el formulario enviado
-    if request.method =='POST':
-        #Mandaron el formulario
-        form =productoForm(request.POST)
-        #Validar que el formulario este correcto 
+def agregar_producto(request):
+    #checar si vengo desde el formulario enviado
+    if request.method == 'POST':
+        #mandaron el formulario
+        form = productoForm(request.POST)
+        #validar que el formulario este correcto
         if form.is_valid():
-            #Si es valido entonces lo guardo
+            #si es valido entonces lo guardo
             form.save()
-            return redirect('ver') #Redirecciona a la lista de productos
-    #Si no vengo de enviar el formulario (si solo quiero ver)
-    else :
-        form=productoForm()
-    #Vamos a pintar el formulario
-    return render(request,'agregar_productos.html',{'form':form})    
+            return redirect('ver') #redirecciona a la lista de productos
+    #si no vengo de enviar el formulario
+    else: 
+        form = productoForm()
+    #vamos a pintar el formulario
+    return render(request, 'agregar_productos.html', {'form': form})
+
+
+
+#función que agrega productos desde una llamada asincrona
+import json
+#etiqueta para vitar el uso de CSRF
+#@csrf_exempt <-- Evitar su uso a menos que estemos probando
+
+def registrar_producto(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)  # Función que toma texto crudo y lo convierte a JSON
+            producto = Producto.objects.create(
+                nombre=data['nombre'],
+                precio=data['precio'],
+                imagen=data['imagen']
+            )  # Estamos construyendo la instancia y guardándola en la BD
+            return JsonResponse({
+                'mensaje': 'Registro exitoso!',
+                'id': producto.id
+            }, status=201)
+        except Exception as e:
+            return JsonResponse({
+                'Error': 'Ocurrió un error: ' + str(e)
+            }, status=400)
+    return JsonResponse({
+        'Error': 'Método no soportado'
+    }, status=405)
+
+
 
 def ver_producto(request):
-    #Obtener de la base de datos todoso los productos
-    productos=Producto.objects.all()
-    #Estamos de volviendo u front un obejto desde el back
-    return  render (request,'ver.html',{'productos':productos})
+    #Obtener de la BD todos los productos
+    productos = Producto.objects.all()
+    #Esramos devolviendo al front un objeto desde el back
+    return render(request, 'ver.html', {'productos': productos})
 
-#Esta funcion pinta el html que carga los productos con json
+#esta funcion pinta el html que carga los productos con JSON
 def index(request):
-    return render(request,'productos.html')
+    return render(request, 'productos.html')
 
-#esta funcion que devuelve todod los productos como un JSON
+#Devolver todos los productos como un JSON
 def lista_productos(request):
-    productos=Producto.objects.all()
-     #Generar un diccionario al aire que ordena los productos
+    productos = Producto.objects.all()
 
-    data=[
-         {
-            'nombre':p.nombre,
-            'precio':p.precio,
-            'imagen':p.imagen,
-         }
-         for p in productos
-     ]
-    return JsonResponse(data,safe=False)
+    #Generar un diccionario al aire que ordene los productos
+    data = [
+        {
+            'nombre': p.nombre,
+            'precio': p.precio,
+            'imagen': p.imagen
+        }
+
+        for p in productos
+    ]
+
+    return JsonResponse(data, safe=False)
